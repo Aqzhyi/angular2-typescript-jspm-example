044203d4bccdf81b121d4f0adba5bdcbea717aca
pleasurazy <pleasurazy@gmail.com>
2015-10-03 13:07:19 +0800

    gulp watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,8 +5,12 @@ let plugins = gulploadplugins()
 
 let tsProject = plugins.typescript.createProject('./tsconfig.json')
 
+gulp.task('watch', () => {
+  gulp.watch('./src/**/*.ts', ['build'])
+})
+
 gulp.task('build', (done) => {
-  return gulp.src('./src/*.ts')
+  return gulp.src('./src/**/*.ts')
   .pipe(plugins.plumberNotifier())
   .pipe(plugins.sourcemaps.init())
   .pipe(plugins.typescript(tsProject))
